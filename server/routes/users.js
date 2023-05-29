0ef27f7bcc2b6e7a4903d83a4bebfbc0a5c77913
Moksh Agrawal <111683521+Moksh-10@users.noreply.github.com>
2023-05-29 20:04:09 +0530

    Add files via upload

---
+++ server/routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriends,
+}from "../controllers/users.js"
+import { verifyToken } from "../middleware/auth.js";
+import { get } from "mongoose";
+
+const router = express.Router();
+
+/* Read */
+router.get("/:id",verifyToken,getUser);
+router.get("/:id/friends",verifyToken,getUserFriends);
+
+/* Update */
+router.patch("/:id/:friendId",verifyToken,addRemoveFriends);
+
+export default router;
